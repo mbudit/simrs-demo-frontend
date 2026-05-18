2e4529384ffa6b0c9be1fd05a5dbb2a77137db2d
mbudit <buditriono13@gmail.com>
2026-05-18 07:50:44 +0700

    fix: dupe dependencies declared

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -18,9 +18,7 @@ import ReportPage from './pages/ReportPage'
 import EmergencyPage from './pages/EmergencyPage'
 import EmergencyAssessmentPage from './pages/EmergencyAssessmentPage'
 import EmergencyMedicalRecordDetailPage from './pages/EmergencyMedicalRecordDetailPage'
-import EmergencyPage from './pages/EmergencyPage'
-import EmergencyAssessmentPage from './pages/EmergencyAssessmentPage'
-import EmergencyMedicalRecordDetailPage from './pages/EmergencyMedicalRecordDetailPage'
+
 
 function App() {
   return (
@@ -52,8 +50,7 @@ function App() {
       <Route path="/laporan" element={<ReportPage />} />
       <Route path="/igd" element={<EmergencyPage />} />
       <Route path="/igd/asesmen/:id" element={<EmergencyAssessmentPage />} />
-      <Route path="/igd" element={<EmergencyPage />} />
-      <Route path="/igd/asesmen/:id" element={<EmergencyAssessmentPage />} />
+
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
